45289be20dddfa7406b67a0d7fa23ecb4a4f2bcc
Tazim Sikder <tazimsikder100@gmail.com>
2023-06-06 22:06:41 -0400

    changes

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
-import { TypingText } from '../components';
+import { motion } from "framer-motion";
+import { TypingText } from "../components";
 
-import styles from '../styles';
-import { fadeIn, staggerContainer } from '../utils/motion';
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
 
 const About = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -19,25 +19,26 @@ const About = () => (
       <TypingText title="| About Planets" textStyles="text-center" />
 
       <motion.p
-        variants={fadeIn('up', 'tween', 0.2, 1)}
+        variants={fadeIn("up", "tween", 0.2, 1)}
         className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       >
-        <span className="font-extrabold text-white">Exploring Planets</span> is a new
-        thing in the future, where you can enjoy different worlds by travelling with a 
-        space ship that is faster than the speed of light, because this is really the{' '}
+        <span className="font-extrabold text-white">Exploring Planets</span> is
+        a new thing in the future, where you can enjoy different worlds by
+        travelling with a space ship that is faster than the speed of light,
+        because this is really the{" "}
         <span className="font-extrabold text-white">
           madness of the universe
-        </span>{' '}
-        of today, using only{' '}
+        </span>{" "}
+        of today, using only{" "}
         <span className="font-extrabold text-white">Super</span> devices you can
         easily explore the planets world you want, turn your dreams into
-        reality. Let's{' '}
+        reality. Let's{" "}
         <span className="font-extrabold text-white">explore</span> the madness
         of the metaverse by scrolling down
       </motion.p>
 
       <motion.img
-        variants={fadeIn('up', 'tween', 0.3, 1)}
+        variants={fadeIn("up", "tween", 0.3, 1)}
         src="/arrow-down.svg"
         alt="arrow down"
         className="w-[18px] h-[28px] object-contain mt-[28px]"
